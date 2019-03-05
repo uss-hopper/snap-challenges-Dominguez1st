8a3b2e602c860ae3ffd02947fd2cb3d334e615e9
Robert Dominguez <dominguez.1st@gmail.com>
2019-03-05 08:36:36 -0700

    I finished snap challenge 17

---
+++ test.js
@@ -0,0 +1,10 @@
+let string = "sdfwgvw sdgrerg egv rg erg irrg  er ei eri erbi";
+
+let array = string.split(" ");
+
+console.log(array);
+
+let newArray = array.filter(function(word) {
+	return
+
+})
